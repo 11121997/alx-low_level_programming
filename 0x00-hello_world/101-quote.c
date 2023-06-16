10ef0331be40c88264400e84758ab044ad024ffe
11121997 <nawalkhaled27@yahoo.com>
2023-06-15 20:07:57 -0700

    l

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,12 @@
 *main-Entry point
 *
 *Description:"print a quote using write function"
-*write(int fd.const void *buf.size_t count)
+*fwrite(int fd. const void *buf.size_t count)
 *Return: 1 (not success)
 */
 int main(void)
 {
 char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1.quo.59)
+fwrite(1, quo, 59);
 return (1);
+}
