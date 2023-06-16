ced4ce1f217baf45c5918bac8ddb93a11c1e2a45
11121997 <nawalkhaled27@yahoo.com>
2023-06-16 06:39:43 -0700

    l

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 /**
 *main-Entry point
 *
-*Description:"print a quote using scanf function"
+*Description:"print a quote using scanf function with printf"
 *
 *Return: 1 (not success)
 */
 int main(void)
 {
 scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+ptintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
