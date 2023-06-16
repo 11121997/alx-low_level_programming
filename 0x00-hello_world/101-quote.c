4593fe81ff2430ec0aef63fea9bcd6d60e24617e
11121997 <nawalkhaled27@yahoo.com>
2023-06-16 06:17:46 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,12 @@
 /**
 *main-Entry point
 *
-*Description:"print a quote using getch function"
+*Description:"print a quote using scanf function"
 *
 *Return: 1 (not success)
 */
 int main(void)
 {
-getch("and that piece ");
-getch("of art is usefu");
-getch("l\" - Dora Korpa");
-getch("r, 2015-10-19\n");
+scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");	
 return (1);
 }
