1ea8b6b86ce08f6b3e85579fa6f8ec15ed9cd422
11121997 <nawalkhaled27@yahoo.com>
2023-06-15 17:32:21 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
-#!/BIN/BASH
-include <stdio>
-int main(void){
-	puts("Programming is like building a multilingual puzzle);
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Desciption: A C program that prints with puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
-retun(0)
