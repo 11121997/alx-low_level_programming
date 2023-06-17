d1bff7de05502aad046fa0680b290a41b6932a60
11121997 <nawalkhaled27@yahoo.com>
2023-06-17 11:56:02 -0700

    m

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+*main-Entry point
+*
+*Description: A C program print combination numbers
+*
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int n1 = 0, n2, n3;
+while (n1 <= 7)
+{
+n2 = 0;
+while (n2 <= 8)
+{
+n3 = 0;
+while (n3 <= 9)
+{
+if (n1 != n2 && n1 < n2 && n1 != n3 && n1 < n3)
+{
+putchar(n1 + '0');
+putchar(n2 + '0');
+putchar(n3 + '0');
+if (n1 + n2 + n3 != 24)
+{
+putchar(',');
+putchar(' ');
+}
+}
+n3++;
+}
+n2++;
+}
+n1++;
+}
+putchar('\n');
+return (0);
+}
