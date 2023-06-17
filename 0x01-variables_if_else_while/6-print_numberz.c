a4fa79b6312238dcdd5145855d2a63c5fe8363f0
11121997 <nawalkhaled27@yahoo.com>
2023-06-16 18:50:51 -0700

    m

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*main-Enrty point
+*
+*Description: A C program print numbers
+*
+*Return:Always 0 (success)
+*/
+
+int main(void)
+{
+int n = 0;
+while (n < 10)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
