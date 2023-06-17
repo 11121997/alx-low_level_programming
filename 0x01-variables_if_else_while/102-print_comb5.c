508056659c4079e3df72bad2c172a847d702edcf
11121997 <nawalkhaled27@yahoo.com>
2023-06-17 12:35:53 -0700

    m

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,7 +13,7 @@ int main(void)
 int n1 = 0, n2;
 while (n1 <= 99)
 {
-n2 = n1 + 1;
+n2 = n1;
 while (n2 <= 99)
 {
 if (n2 != n1)
@@ -22,7 +22,7 @@ putchar((n1 / 10) + '0');
 putchar((n1 % 10) + '0');
 putchar(' ');
 putchar((n2 / 10) + '0');
-putchar((n2 / 10) + '0');
+putchar((n2 % 10) + '0');
 if (n1 != 98 || n2 != 99)
 {
 putchar(',');
