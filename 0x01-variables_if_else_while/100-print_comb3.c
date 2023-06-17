b7d2782500a3b31c6baa00ef9c10c08bd302950e
11121997 <nawalkhaled27@yahoo.com>
2023-06-17 07:20:42 -0700

    m

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,12 +13,11 @@ int main(void)
 int n1 = 0, n2 = 1;
 while (n1 <= 9 && n2 <= 9)
 {
+while (n1 != n2 || n1 > n2)
 putchar(n1 + '0');
-n1++;
 putchar(n2 + '0');
+n1++;
 n2++;
-if (n1 == n2 && n1 > n2)
-break;
 if (n1 + n2 != 17)
 break;
 }
