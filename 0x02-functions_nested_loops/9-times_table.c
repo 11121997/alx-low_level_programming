c528c9c7c76121d691928250f177074ebc536358
11121997 <nawalkhaled27@yahoo.com>
2023-06-21 11:05:23 -0700

    m

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,18 +10,17 @@ int x, y, z;
 for (x = 0; x <= 9; x++)
 {
 _putchar(0 + '0');
-
-for (y = 0; y <= 9; y++);
+for (y = 0; y <= 9; y++)
 {
 _putchar(',');
 _putchar(' ');
-z = x * y
-if (x <= 9);
-_putchar(' ')
+z = x * y;
+if (x <= 9)
+_putchar(' ');
 else
 _putchar((z / 10) + '0');
-_putchar((z % 10) + '0')
+_putchar((z % 10) + '0');
 }
 _putchar('\n');
 }
-}       
+}
