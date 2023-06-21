654c2834bc55b48de9ca2fd596ce63c1ff7587d7
11121997 <nawalkhaled27@yahoo.com>
2023-06-21 16:42:32 -0700

    l

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,11 +9,11 @@ void print_times_table(int n)
 int x, y, z;
 if (n <= 15 && n >= 0)
 {
-for (x = 0; x <= n; n++)
-
+for (x = 0; x <= n; x++)
+{
 _putchar(0 + '0');
 for (y = 1; y <= n; y++)
-
+{
 _putchar(',');
 _putchar(' ');
 z = x * y;
@@ -29,7 +29,8 @@ _putchar((z / 10) % 10 + '0');
 else if (z <= 99 && z >= 10)
 _putchar((z / 10) + '0');
 _putchar((z % 10) + '0');
-
+}
 _putchar('\n');
+}
 }
 }
