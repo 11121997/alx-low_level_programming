bb768fc09ba6614931cbd7d52fa9d9d8cdddb348
11121997 <nawalkhaled27@yahoo.com>
2023-06-21 15:36:49 -0700

    l

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,9 +6,8 @@
 *@x: a character that print integer number
 */
 
-void print_to_98(int n)
-{
-	int x;
+void print_to_98(int n, int x)
+	{
 if (n > 98)
 {
 for (x = n; x > 98; x--)
@@ -19,4 +18,5 @@ for (x = n; x < 98; x++)
 printf("%d, ", x);
 
 printf("98\n");
+}
 }
