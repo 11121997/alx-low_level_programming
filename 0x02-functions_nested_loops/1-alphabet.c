2a7b8a5a344c1d24cdcf583d28df223db812414d
11121997 <nawalkhaled27@yahoo.com>
2023-06-20 17:41:40 -0700

    l

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,15 @@
 *
 *Description: A C program to print alphabets wit _putchar functionj
 *
+*Return: Always 0 (success)
 */
-
-void print_alphabet(void);
+int main(void)
 {
+void print_alphabet(void);
+
 int ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 _putchar(ch);
 _putchar('\n');
+return (0);
 }
