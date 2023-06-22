2685ac9fe1c1368f78f790402059398904570f19
11121997 <nawalkhaled27@yahoo.com>
2023-06-21 17:59:42 -0700

    l

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Description: prints the sum of the even-valued terms
+*
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+long x, y, sum, total;
+while (1)
+{
+sum = x + y;
+if (sum > 4000000)
+break;
+if ((sum % 2) == 0)
+total += sum;
+x = y;
+y = sum;
+}
+printf("%ld\n", total);
+return (0);
+}
