996cc001d27a40f5cb4a2539a3482117c777911a
11121997 <nawalkhaled27@yahoo.com>
2023-06-21 20:04:17 -0700

    lk

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,18 +12,36 @@ int main(void)
 {
 int n;
 long x = 0, y = 1, sum;
-for (n = 0; n < 98; n++)
+long hx, fx, hy, fy;
+long h1, h2;
+for (n = 0; n < 92; n++)
 {
 sum = x + y;
 printf("%ld", sum);
 x = y;
 y = sum;
-if (sum == 97)
+}
+hx = x / 10000000000;
+hy = y / 10000000000;
+fx = x % 10000000000;
+fy = y % 10000000000;
+for (n = 93; n < 99; n++)
 {
+	h1 = hx + hy;
+		h2 = fx + fy;
+		if (fx + fy > 9999999999)
+		{
+			h1 += 1;
+			h2 %= 10000000000;
+		}
+		printf("%ld%ld", h1, h2);
+		if (n != 98)
+			printf(", ");
+		hx = hy;
+		fx = fy;
+		hy = h1;
+		fy =h2;
+}
 printf("\n");
-}
-else
-printf(", ");
-}
 return (0);
 }
