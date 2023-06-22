0b1202c9303fa3281ab670f53d5bf8c29307392e
11121997 <nawalkhaled27@yahoo.com>
2023-06-21 19:24:29 -0700

    lk

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Description: prints the first 98 Fibonacci numbers
+*
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int n;
+long x = 0, y = 1, sum;
+for (n = 0; n < 98; n++)
+{
+sum = x + y;
+printf("%ld", sum);
+if (sum == 98)
+{
+printf("\n");
+}
+else
+printf(", ");
+}
+return (0);
+}
