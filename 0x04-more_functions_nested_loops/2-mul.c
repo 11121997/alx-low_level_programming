35cf22ad52773a7a9eccb9023524ee9228895b3d
11121997 <nawalkhaled27@yahoo.com>
2023-06-23 11:57:38 -0700

    k

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*mul - function that multiplies two integers
+*@a: character take an integer number
+*@b: character take an integer number
+*
+*Return: Always 0 (success)
+*/
+
+int mul(int a, int b)
+{
+	printf("%d", a * b);
+	return (0);
+}
