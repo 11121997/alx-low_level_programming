65d15be00e763cfa70b8145e0a132330de457418
11121997 <nawalkhaled27@yahoo.com>
2023-06-23 15:26:04 -0700

    l

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,20 @@
 
 void more_numbers(void)
 {
-	int ln, i;
+	int ln, i, n;
 
 	for (ln = 0; ln <= 10; ln++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			_putchar(i + '0');
+		n = i;
+	if (i > 9)
+	{
+	_putchar(1 + '0');
+	n = i % 10;
+	}
+	_putchar(n + '0');
 		}
-		_putchar(ln + '0');
-			_putchar('\n');
+		_putchar('\n');
 	}
 }
