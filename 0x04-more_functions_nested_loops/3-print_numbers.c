2d618f103e390d8792f4babe7f88ba9eabd37d10
11121997 <nawalkhaled27@yahoo.com>
2023-06-23 12:42:11 -0700

    l

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,12 +2,12 @@
 
 /**
 *print_numbers - function that prints the numbers
-*@i: character take an integer number
 */
 
 void print_numbers(void)
 {
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
 		_putchar(i + '0');
