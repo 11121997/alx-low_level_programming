a2ad9621341b7d9c9862fedda9c608a56c91aecc
11121997 <nawalkhaled27@yahoo.com>
2023-06-23 18:51:43 -0700

    k

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,25 +14,26 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if ((n % 3) == 0)
-		{
-			printf("Fizz");
-		}
-		else if ((n % 5) == 0)
-		{
-			printf("Buzz");
-		}
-		else if (((n % 3) == 0) && ((n % 5) == 0))
+		if (((n % 3) == 0) && ((n % 5) == 0))
 		{
 			printf("FizzBuzz");
 		}
-		else
-			printf("%d", n);
+		else if (((n % 3) == 0) && !((n % 5) == 0))
+		{
+			printf("Fizz");
+		}
+		else if (((n % 5) == 0) && !((n % 3) == 0))
+		{
+			printf("Buzz");
+		}
 		if (n != 100)
-			{
-				printf(" ");
-			}
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf("\n");
+		}
 	}
-	printf("\n");
 	return (0);
 }
