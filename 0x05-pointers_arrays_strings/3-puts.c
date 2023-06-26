dd972fb8df89b0a176414eaf92db73c8047a923a
11121997 <nawalkhaled27@yahoo.com>
2023-06-26 16:33:46 -0700

    k

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,14 +2,14 @@
 
 /**
 *_puts - function that prints a string
-*@str: string input parameter
+*@str: string parameter to print
 */
 
 void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-		_putchar(*str + '0');
+		_putchar(*str + 0);
 		str++;
 	}
 	_putchar('\n');
