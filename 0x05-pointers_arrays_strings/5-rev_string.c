5c3807fe9c3b3aee0e88c4a8e7a23255680d16f7
11121997 <nawalkhaled27@yahoo.com>
2023-06-26 16:59:55 -0700

    k

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,15 +8,15 @@
 void rev_string(char *s)
 {
 	int l, i;
-	char temp;
+	char tmp;
 
-	for (l = 0; s[l] != '\0'; ++l)
-		;
+	while (s[i++])
+		l++;
 
-	for (i = 0; i < 1 / 2; i++)
+	for (i = l - 1; i >= l / 2; i--)
 	{
-		temp = s[i];
+		tmp = s[i];
 		s[i] = s[l - 1 - i];
-		s[l - 1 - i] = temp;
+		s[l - 1 -i] = tmp;
 	}
 }
