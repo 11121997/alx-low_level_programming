6350e2a4fedda54eb7866fa133c7c40bbc59feb7
11121997 <nawalkhaled27@yahoo.com>
2023-06-26 18:53:23 -0700

    l

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdib.h>
+#include <time.h>
+/**
+*main - program that generates random valid passwords
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int sum;
+	char c;
+	
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
