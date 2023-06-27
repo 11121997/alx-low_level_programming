dbd98f65737f485333b63679048085825dbfe72f
11121997 <nawalkhaled27@yahoo.com>
2023-06-26 19:11:49 -0700

    l

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,16 +8,44 @@
 
 int main(void)
 {
-	int sum;
-	char c;
-	
-	srand(time(NULL));
-	while (sum <= 2645)
+	char pass[84];
+	int index = 0, sum = 0, dffh1, dffh2;
+
+	srand(time(0));
+
+	while (sum < 2772)
 	{
-		c = rand() % 128;
-		sum += c;
-		putchar(c);
+		pass[index] = 33 + rand() % 94;
+		sum += pass[index++];
 	}
-	putchar(sum - 2772);
+
+	pass[index] = '\0';
+
+	if (sum != 2772)
+	{
+		dffh1 = (sum - 2772) / 2;
+		dffh2 = (sum - 2772) / 2;
+
+		if ((sum - 2772) % 2 != 0)
+			dffh1++;
+
+		for (index = 0; pass[index]; index++)
+		{
+			if (pass[index] >= (33 + dffh1))
+			{
+				pass[index] -= dffh1;
+				break;
+			}
+		}
+		for (index = 0; pass[index]; index++)
+		{
+			if (pass[index] >= (33 + dffh2))
+			{
+				pass[index] -= dffh2;
+				break;
+			}
+		}
+	}
+	printf("%s", pass);
 	return (0);
 }
