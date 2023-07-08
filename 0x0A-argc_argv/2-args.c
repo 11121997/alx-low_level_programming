d04165b9e1ccfba1f8f9d54573c1fad8793f5620
11121997 <nawalkhaled27@yahoo.com>
2023-07-07 19:24:00 -0700

    arg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*@argc: int
+*@argv: string array
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
