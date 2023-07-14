77c3dc71ffc88cb81259eeb7d113e179f47b5dee
11121997 <nawalkhaled27@yahoo.com>
2023-07-13 17:05:01 -0700

    concatenates

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,9 +22,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		;
 	p = malloc(l_s1 + n + 1);
 	if (p == NULL)
+	{
 		return (NULL);
+	}
 	for (i = 0; s1[i] != '\0'; i++)
+	{
 		p[i] = s1[i];
+	}
 	for (j = 0; s2[j] != '\0'; j++)
 	{
 		p[i] = s2[j];
