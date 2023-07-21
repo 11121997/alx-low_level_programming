fd8b9c4f8ac9c348af800df0d3777517c8bc0604
11121997 <nawalkhaled27@yahoo.com>
2023-07-20 18:07:51 -0700

    hhhaaarrrddd

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,43 @@
+#include "variadic_functions.h"
+
+/**
+*print_all - function that prints anything
+*@format: list of types of arguments
+*/
+
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *str, *separator = "";
+
+	va_list list;
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", separator, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", separator, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", separator, va_arg(list, double));
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", separator, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			separator = ", ";
+			i++;
+		}
+	}
+}
