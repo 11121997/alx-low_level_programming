73706be3c254f1c446ee3074a7fac4067cb0d24a
11121997 <nawalkhaled27@yahoo.com>
2023-07-28 19:24:37 -0700

    linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,35 +1,3 @@
 #include "lists.h"
 
-/**
-*_strlen - function find length of string
-*@s: string to check it's length
-*Return: length of string
-*/
-int _strlen(char *s)
-{
-	int i = 0;
 
-	if (!s)
-		return (0);
-	while (*s++)
-		i++;
-	return (i);
-}
-
-/**
-*print_list - function that prints all the elements of a list_t list
-*@h: pointer to first node
-*Return: number of nodes
-*/
-size_t print_list(const list_t *h)
-{
-	size_t i = 0;
-
-	while (h)
-	{
-		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
-		h = h->next;
-		i++;
-	}
-	return (i);
-}
---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+void main_constructor(void) __attribute__((constructor));
+
+/**
+*main_constructor - constructor function
+*/
+void main_constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n);
+}
