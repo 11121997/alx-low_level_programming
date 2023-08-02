ca3268974f956db1d02b681c30f0c988d8993a9a
11121997 <nawalkhaled27@yahoo.com>
2023-08-02 10:56:59 -0700

    hard link

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,40 +3,30 @@
 /**
 **insert_nodeint_at_index - function that inserts a new node
 *@head: pointer to first node
-*@idx: the index of the list
-*@n: integer data
+*@idx: index of the node
+*@n: integer value
 *Return: the address of the new node
 */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *node, *newnode = malloc(sizeof(listint_t));
-	int i = 0;
+	listint_t *new_node = malloc(sizeof(listint_t)), *temp;
+	unsigned int i;
 
-	if (!head || !newnode)
-		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
 
-	newnode->n = n;
-	newnode->next = NULL;
-	if (!idx)
+	if (idx == 0)
 	{
-		newnode->next = *head;
-		*head = newnode;
-		return (newnode);
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
-
-	node = *head;
-	while (node)
+	temp = head;
+	for (i = 0; i < idx - 1; i++)
 	{
-		if (i == idx - 1)
-		{
-			newnode->next = node->next;
-			node->next = newnode;
-			return (newnode);
-		}
-		i++;
-		node = node->next;
+		temp = temp->next;
 	}
-	free(newnode);
-	return (NULL);
+	new_node->next = temp->next;
+	temp->next = new_node;
+	return (new_node);
 }
