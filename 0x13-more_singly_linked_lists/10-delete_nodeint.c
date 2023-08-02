026813e74e6e4a4ff8de14811a6cf47344e6198e
11121997 <nawalkhaled27@yahoo.com>
2023-08-02 11:43:54 -0700

    more linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,6 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	node = *head;
 	while (node)
 	{
+		prev = node;
 		if (i == index)
 		{
 			prev->next = node->next;
@@ -30,8 +31,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			return (1);
 		}
 		i++;
-		node = node->next;
 		prev->next = node;
+		node = node->next;
 	}
 	return (-1);
 }
