ba11a6daa4659d75d11560e8aa0d22b3468a2f1d
11121997 <nawalkhaled27@yahoo.com>
2023-08-01 17:10:50 -0700

    likedlists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+*listint_len - function that returns the number of elements
+*@h: pointer to first node
+*Return: number of elements
+*/
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
