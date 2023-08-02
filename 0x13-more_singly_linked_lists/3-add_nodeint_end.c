395a020f839c04f6cce86a9239ff9d44aa154631
11121997 <nawalkhaled27@yahoo.com>
2023-08-01 17:36:21 -0700

    likedlists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,11 +12,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new_node = malloc(sizeof(listint_t));
 	listint_t *node;
 
-	new_node->next = NULL;
-	new_node->n = n;
-
 	if (!head || !new_node)
 		return (NULL);
+
+	new_node->next = NULL;
+        new_node->n = n;
 	if (!*head)
 		*head = new_node;
 	else
