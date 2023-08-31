6aae53a06f05d8fa56c594fee7269f9639bc65be
11121997 <nawalkhaled27@yahoo.com>
2023-08-31 12:18:27 -0700

    my update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,9 +9,11 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n = 0;
 
-	if (*b == NULL)
+	if (b == NULL)
 		return (0);
-	if (*b != 0 || *b != 1)
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
 		return (0);
 	n = n * 2 + (*b++ - '0');
 	return (n);
