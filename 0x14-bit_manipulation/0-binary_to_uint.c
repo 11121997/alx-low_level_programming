f2b9a17f6ee42ec57c825c1f46e28bb26212236e
11121997 <nawalkhaled27@yahoo.com>
2023-08-31 12:11:49 -0700

    my update

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function that convert binary to decimal
+ * @b: pointer to 0 or 1
+ * Return: converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0;
+
+	if (*b == NULL)
+		return (0);
+	if (*b != 0 || *b != 1)
+		return (0);
+	n = n * 2 + (*b++ - '0');
+	return (n);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+my bit wise project
