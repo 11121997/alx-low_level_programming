542201099eea26dd3d2a776d4eb016afafb5a913
11121997 <nawalkhaled27@yahoo.com>
2023-09-04 13:42:35 -0700

    my update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,15 +25,15 @@ int _strlen(char *s)
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t bytes = 0, length = _strlen(text_content);
+	ssize_t bytes = 0, len = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	while (length)
-		bytes = write(fd, text_content, length);
+	while (len)
+		bytes = write(fd, text_content, len);
 	close(fd);
-	return (bytes == length ? 1 : -1);
+	return (bytes == len ? 1 : -1);
 }
