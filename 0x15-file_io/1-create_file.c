17ffe78183052f3f2283744c9a32047481e1f6ce
11121997 <nawalkhaled27@yahoo.com>
2023-09-04 16:29:45 -0700

    my update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	bytes = write(fd, text_content, len);
 	if (fd == -1 || bytes == -1)
 		return (-1);
