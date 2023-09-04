f4b31c63204564de7360fbde5ec0ae419ec9473c
11121997 <nawalkhaled27@yahoo.com>
2023-09-04 14:59:49 -0700

    my update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,6 +11,7 @@ int _strlen(char *s)
 
 	if (!s)
 		return (0);
+
 	if (*s++)
 		i++;
 	return (i);
@@ -27,7 +28,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t bytes = 0, len = _strlen(text_content);
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
