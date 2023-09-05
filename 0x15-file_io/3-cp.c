ae015a5d09681b44f38216bf51a7e382f37c4fbc
11121997 <nawalkhaled27@yahoo.com>
2023-09-04 17:42:32 -0700

    my update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,6 +4,7 @@
 #define ERR_NOREAD "Error: Can't read from file %s\n"
 #define ERR_NOWRITE "Error: Can't write to NAME_OF_THE_FILE %s\n"
 #define ERR_NOCLOSE "Error: Can't close fd %d\n"
+#define PERMISSIONS(S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
 /**
  * main - Entry point
@@ -21,7 +22,7 @@ int main(int ac, char **av)
 	from_fd = open(av[1], O_RDONLY);
 	if (from_fd == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
-	to_fd = open(av[1], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	to_fd = open(av[1], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (to_fd == -1)
 		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 	while ((bytes = read(from_fd, buf, READ_BUF_SIZE)) > 0)
