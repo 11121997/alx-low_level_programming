7ccd3b7bc12bda65ac00cb63d3a79171c7f6d4e9
11121997 <nawalkhaled27@yahoo.com>
2023-09-04 17:59:51 -0700

    my update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,8 +18,8 @@ int main(int ac, char **av)
 	ssize_t bytes;
 	char buf[READ_BUF_SIZE];
 
-		if (ac != 3)
-			dprintf(STDERR_FILENO, USAGE), exit(97);
+	if (ac != 3)
+		dprintf(STDERR_FILENO, USAGE), exit(97);
 	from_fd = open(av[1], O_RDONLY);
 	if (from_fd == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
