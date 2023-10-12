0c90faa22b05726291028dd1b3aa229a3b44d919
11121997 <nawalkhaled27@yahoo.com>
2023-10-12 07:46:17 -0700

    update

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - function that frees a dlistint_t list
+ * @head: first node
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+
+	while (*head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
