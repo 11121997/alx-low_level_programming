b26ad7a17092dddb104385ab02d71cb64b78df02
11121997 <nawalkhaled27@yahoo.com>
2023-10-16 03:32:44 -0700

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 wget -P /alx-low_level_programming/0x18-dynamic_libraries https://github.com/11121997/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/alx-low_level_programming/0x18-dynamic_libraries/libgiga.so
