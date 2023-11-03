259fa97d4389f2688a3cc20b70abb6dbe2f0139a
11121997 <nawalkhaled27@yahoo.com>
2023-11-03 06:35:21 -0700

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,8 +7,8 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i;
 	hash_table_t *hash_table;
+	unsigned long int i;
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
