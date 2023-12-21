ddf069936c54b2001ee22add828633ea8920ddfc
11121997 <nawalkhaled27@yahoo.com>
2023-12-21 04:31:35 -0800

    update

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#Island Perimeter module
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    C = 0
+    S = 0
+
+    for h in range(height):
+        for k in range(width):
+            if grid[h][k] == 1:
+                S += 1
+                if (k > 0 and grid[h][k - 1] == 1):
+                    edges += 1
+                if (h > 0 and grid[h - 1][k] == 1):
+                    edges += 1
+
+    C = S * 4 - edges * 2
+    return C
